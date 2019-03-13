b855a92409885fff057896b99004ff814e617fd8
François Steinmetz <fs@hygeos.com>
2019-03-13 11:03:40 +0100

    Fixes in NetCDF HICO reader
    * deal wuth maked arrays
    * fix unit of TOA radiance

--- polymer/level1_hico_netcdf.py
+++ polymer/level1_hico_netcdf.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function, division, absolute_import
 from polymer.level1 import Level1_base
+from polymer.level1_nasa import filled
 from polymer.block import Block
 from polymer.hico import bands_hico, wav_hico, F0_hico
 import numpy as np
@@ -29,18 +30,14 @@ class Level1_HICO(object):
     def __init__(self, filename, blocksize=200,
                  sline=0, eline=-1, scol=0, ecol=-1,
                  ancillary=None, landmask=None):
-        self.nc=Dataset(filename)         #self.h5 = h5py.File(filename) #original h5 code
+        self.nc=Dataset(filename)
         self.sensor = 'HICO'
         self.filename = filename
         self.landmask = landmask
-        
-        self.Lt = self.nc.groups['products']['Lt']  #self.Lt = self.h5['products']['Lt']  #original h5 code
-        #print(self.Lt) 
+
+        self.Lt = self.nc.groups['products']['Lt']
 
         self.totalheight, self.totalwidth, nlam = self.Lt.shape
-        #print(self.totalheight) 
-        #print(self.totalwidth) 
-        #print(self.Lt.shape) 
 
         self.blocksize = blocksize
         self.sline = sline
@@ -50,67 +47,49 @@ class Level1_HICO(object):
             self.ancillary = Ancillary_NASA()
         else:
             self.ancillary = ancillary
-        #print(self.ancillary)
-        
+
         if eline < 0:
             self.height = self.totalheight
             self.height -= sline
             self.height += eline + 1
         else:
             self.height = eline-sline
-        #print(eline)
-        
+
         if ecol < 0:
             self.width = self.totalwidth
             self.width -= scol
             self.width += ecol + 1
         else:
             self.width = ecol - scol
-        #print(ecol)
-        
+
         self.shape = (self.height, self.width)
         print('Initializing HICO product of size', self.shape)
 
         self.datetime = self.get_time()
-        #print(self.datetime)
 
         # initialize ancillary data
         self.ozone = self.ancillary.get('ozone', self.datetime)
-        #print(self.ozone,"is of type", type(self.ozone))
         self.wind_speed = self.ancillary.get('wind_speed', self.datetime)
-        #print(self.wind_speed,"is of type", type(self.wind_speed))
         self.surf_press = self.ancillary.get('surf_press', self.datetime)
-        #print(self.surf_press,"is of type", type(self.surf_press))
 
         self.ancillary_files = OrderedDict()
         self.ancillary_files.update(self.ozone.filename)
-        #print(self.ozone.filename)
         self.ancillary_files.update(self.wind_speed.filename)
-        #print(self.wind_speed.filename)
         self.ancillary_files.update(self.surf_press.filename)
-        #print(self.surf_press.filename)
         self.init_landmask()
-        #print(self.init_landmask)
-        #import sys
-        #sys.exit(1)
 
 
     def init_landmask(self):
         if not hasattr(self.landmask, 'get'):
             return
-        #lat = self.h5['navigation']['latitudes'][:,:] #original h5 code
-        #lon = self.h5['navigation']['longitudes'][:,:] #original h5 code
-        
-        lat=self.nc.groups['navigation']['latitudes'][:,:]
-        lon=self.nc.groups['navigation']['longitudes'][:,:]
+        lat = filled(self.nc.groups['navigation']['latitudes'][:,:])
+        lon = filled(self.nc.groups['navigation']['longitudes'][:,:])
 
         self.landmask_data = self.landmask.get(lat, lon)
-    
-    
+
+
     def get_time(self):
-        #beg_date = self.h5['metadata']['FGDC']['Identification_Information']['Time_Period_of_Content'].attrs['Beginning_Date'].decode('ascii') #original h5 code
-        #beg_time = self.h5['metadata']['FGDC']['Identification_Information']['Time_Period_of_Content'].attrs['Beginning_Time'].decode('ascii') #original h5 code
-        metadata=self.nc.groups['metadata']['FGDC']['Identification_Information']['Time_Period_of_Content']
+        metadata = self.nc.groups['metadata']['FGDC']['Identification_Information']['Time_Period_of_Content']
         beg_date = metadata.getncattr('Beginning_Date')
         beg_time = metadata.getncattr('Beginning_Time')
         return datetime.strptime(beg_date + beg_time, '%Y%m%d%H%M%S')
@@ -128,37 +107,27 @@ class Level1_HICO(object):
         block.jday = self.datetime.timetuple().tm_yday
         block.month = self.datetime.timetuple().tm_mon
 
-        #block.latitude = self.h5['navigation']['latitudes'][SY, SX]  #original h5 code
-        # block.longitude = self.h5['navigation']['longitudes'][SY, SX]  #original h5 code
-        #block.sza = self.h5['navigation']['solar_zenith'][SY, SX]  #original h5 code
-        #block.vza = self.h5['navigation']['sensor_zenith'][SY, SX]  #original h5 code
-        #block.saa = self.h5['navigation']['solar_azimuth'][SY, SX]  #original h5 code
-        #block.vaa = self.h5['navigation']['sensor_azimuth'][SY, SX]  #original h5 code
-
-        block.latitude = self.nc.groups['navigation']['latitudes'][SY, SX]
-        block.longitude = self.nc.groups['navigation']['longitudes'][SY, SX]
-        block.sza = self.nc.groups['navigation']['solar_zenith'][SY, SX]
-        block.vza = self.nc.groups['navigation']['sensor_zenith'][SY, SX]
-        block.saa = self.nc.groups['navigation']['solar_azimuth'][SY, SX]
-        block.vaa = self.nc.groups['navigation']['sensor_azimuth'][SY, SX]
+        block.latitude = filled(self.nc.groups['navigation']['latitudes'][SY, SX])
+        block.longitude = filled(self.nc.groups['navigation']['longitudes'][SY, SX])
+        block.sza = filled(self.nc.groups['navigation']['solar_zenith'][SY, SX])
+        block.vza = filled(self.nc.groups['navigation']['sensor_zenith'][SY, SX])
+        block.saa = filled(self.nc.groups['navigation']['solar_azimuth'][SY, SX])
+        block.vaa = filled(self.nc.groups['navigation']['sensor_azimuth'][SY, SX])
         
-        #assert len(self.Lt.attrs['wavelengths']) == len(bands_hico) #original h5 code
         assert len(self.Lt.getncattr('wavelengths')) == len(bands_hico)
         ibands = np.array([bands_hico.index(b) for b in bands])
 
         # read TOA
         block.Ltoa = np.zeros(size3) + np.NaN
-        #slope = self.Lt.attrs['slope']  #original h5 code
-        #intercept = self.Lt.attrs['intercept']  #original h5 code
         slope = self.Lt.getncattr('scale_factor')
         intercept= self.Lt.getncattr('add_offset')
         assert intercept == 0.
-        block.Ltoa = slope * self.Lt[SY, SX, ibands]/10.  # convert
+        assert self.Lt.getncattr('units') == 'W/m^2/micrometer/sr'
+        block.Ltoa = filled(slope * self.Lt[SY, SX, ibands])
+        block.Ltoa *= 10.  # convert W/m^2/um/sr -> mW/cm^2/um/sr
 
         # read bitmask
         block.bitmask = np.zeros(size, dtype='uint16')
-        # flags = self.h5['quality']['flags'][SY, SX]
-        # raiseflag(block.bitmask, L2FLAGS['LAND'], flags & 1 != 0)
 
         # read solar irradiance
         block.F0 = np.zeros(size3) + np.NaN
