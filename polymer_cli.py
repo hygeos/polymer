89506f78582411c4eef446c38a0d1b1b0d76202c
François Steinmetz <fs@hygeos.com>
2023-04-28 10:50:39 +0200

    Fixes in polymer_cli.py

--- polymer_cli.py
+++ polymer_cli.py
@@ -6,19 +6,22 @@
 A basic command line interface for Polymer
 '''
 
-from sys import argv
 import argparse
-from polymer.main import run_atm_corr, Level1, Level2
+from polymer.main import run_atm_corr
+from polymer.level1 import Level1
+from polymer.level2 import Level2
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description=''' Polymer atmospheric correction, simple command line interface.
-                                                     To pass additional parameters, it is advised to execute the
-                                                     function run_atm_corr in a python script''')
+    parser = argparse.ArgumentParser(
+        description='''Polymer atmospheric correction, simple command line interface.
+                       To pass additional parameters, it is advised to execute the
+                       function run_atm_corr in a python script''')
 
     parser.add_argument('input_file')
     parser.add_argument('output_file')
-    parser.add_argument('-fmt', choices=['hdf4', 'netcdf4', 'autodetect'], default='autodetect',
+    parser.add_argument('-fmt', choices=['hdf4', 'netcdf4', 'autodetect'],
+                        default='autodetect',
                         help='Output file format')
     args = parser.parse_args()
 
@@ -28,7 +31,8 @@ if __name__ == "__main__":
         elif args.output_file.endswith('.hdf'):
             args.fmt = 'hdf4'
         else:
-            print('Error, cannot detect file format from output file "{}"'.format(args.output_file))
+            print('Error, cannot detect file format from output file "{}"'.format(
+                args.output_file))
             exit()
 
     run_atm_corr(Level1(args.input_file),
